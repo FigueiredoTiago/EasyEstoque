3efaedf1d07ba0e51dbdce1a8cd8a3e8169cb364
tiago <tiagoandroloko@gmail.com>
2023-09-08 10:11:39 -0300

    ajustes nas medias query da tabela

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,192 +9,187 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <main className="main-home">
-      <div className="info-home">
-        <h1 className="logo">
-          <img src={box} />
-          EasyEstoque
-        </h1>
 
+      <div className="container">
+        <div className="info-home">
+          <h1 className="logo">
+            <img src={box} />
+            EasyEstoque
+          </h1>
         
-
-        <div className="input-box">
-
-          <img src={plus} className="plus" />
-
-          <Link to="/">Novo Produto</Link>
-
-          <input type="text" placeholder="Search Products..." />
-
-          <img src={search} alt="icon search" />
-
+          <div className="input-box">
+            <img src={plus} className="plus" />
+            <Link to="/">Novo Produto</Link>
+            <input type="text" placeholder="Search Products..." />
+            <img src={search} alt="icon search" />
+          </div>
+        </div>
+        <div className="table-container">
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Descrição</th>
+                <th>Preço</th>
+                <th>Quantidade</th>
+                <th>Atualizado</th>
+                <th>Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Produto 1</td>
+                <td>Descrição do Produto 1</td>
+                <td>R$ 19.99</td>
+                <td>100</td>
+                <td>06/09/2023</td>
+                <td> <img src={editar}/> <img src={lixeira}/></td>
+              </tr>
+              <tr>
+                <td>Produto 1</td>
+                <td>Descrição do Produto 1</td>
+                <td>R$ 19.99</td>
+                <td>100</td>
+                <td>06/09/2023</td>
+                <td> <img src={editar}/> <img src={lixeira}/></td>
+              </tr>
+              <tr>
+                <td>Produto 1</td>
+                <td>Descrição do Produto 1</td>
+                <td>R$ 19.99</td>
+                <td>100</td>
+                <td>06/09/2023</td>
+                <td> <img src={editar}/> <img src={lixeira}/></td>
+              </tr>
+              <tr>
+                <td>Produto 1</td>
+                <td>Descrição do Produto 1</td>
+                <td>R$ 19.99</td>
+                <td>100</td>
+                <td>06/09/2023</td>
+                <td> <img src={editar}/> <img src={lixeira}/></td>
+              </tr>
+              <tr>
+                <td>Produto 1</td>
+                <td>Descrição do Produto 1</td>
+                <td>R$ 19.99</td>
+                <td>100</td>
+                <td>06/09/2023</td>
+                <td> <img src={editar}/> <img src={lixeira}/></td>
+              </tr>
+              <tr>
+                <td>Produto 1</td>
+                <td>Descrição do Produto 1</td>
+                <td>R$ 19.99</td>
+                <td>100</td>
+                <td>06/09/2023</td>
+                <td> <img src={editar}/> <img src={lixeira}/></td>
+              </tr>
+              <tr>
+                <td>Produto 1</td>
+                <td>Descrição do Produto 1</td>
+                <td>R$ 19.99</td>
+                <td>100</td>
+                <td>06/09/2023</td>
+                <td> <img src={editar}/> <img src={lixeira}/></td>
+              </tr>
+              <tr>
+                <td>Produto 1</td>
+                <td>Descrição do Produto 1</td>
+                <td>R$ 19.99</td>
+                <td>100</td>
+                <td>06/09/2023</td>
+                <td> <img src={editar}/> <img src={lixeira}/></td>
+              </tr>
+              <tr>
+                <td>Produto 1</td>
+                <td>Descrição do Produto 1</td>
+                <td>R$ 19.99</td>
+                <td>100</td>
+                <td>06/09/2023</td>
+                <td> <img src={editar}/> <img src={lixeira}/></td>
+              </tr>
+              <tr>
+                <td>Produto 1</td>
+                <td>Descrição do Produto 1</td>
+                <td>R$ 19.99</td>
+                <td>100</td>
+                <td>06/09/2023</td>
+                <td> <img src={editar}/> <img src={lixeira}/></td>
+              </tr>
+              <tr>
+                <td>Produto 1</td>
+                <td>Descrição do Produto 1</td>
+                <td>R$ 19.99</td>
+                <td>100</td>
+                <td>06/09/2023</td>
+                <td> <img src={editar}/> <img src={lixeira}/></td>
+              </tr>
+              <tr>
+                <td>Produto 1</td>
+                <td>Descrição do Produto 1</td>
+                <td>R$ 19.99</td>
+                <td>100</td>
+                <td>06/09/2023</td>
+                <td> <img src={editar}/> <img src={lixeira}/></td>
+              </tr>
+              <tr>
+                <td>Produto 1</td>
+                <td>Descrição do Produto 1</td>
+                <td>R$ 19.99</td>
+                <td>100</td>
+                <td>06/09/2023</td>
+                <td> <img src={editar}/> <img src={lixeira}/></td>
+              </tr>
+              <tr>
+                <td>Produto 1</td>
+                <td>Descrição do Produto 1</td>
+                <td>R$ 19.99</td>
+                <td>100</td>
+                <td>06/09/2023</td>
+                <td> <img src={editar}/> <img src={lixeira}/></td>
+              </tr>
+              <tr>
+                <td>Produto 1</td>
+                <td>Descrição do Produto 1</td>
+                <td>R$ 19.99</td>
+                <td>100</td>
+                <td>06/09/2023</td>
+                <td> <img src={editar}/> <img src={lixeira}/></td>
+              </tr>
+              <tr>
+                <td>Produto 1</td>
+                <td>Descrição do Produto 1</td>
+                <td>R$ 19.99</td>
+                <td>100</td>
+                <td>06/09/2023</td>
+                <td> <img src={editar}/> <img src={lixeira}/></td>
+              </tr>
+              <tr>
+                <td>Produto 1</td>
+                <td>Descrição do Produto 1</td>
+                <td>R$ 19.99</td>
+                <td>100</td>
+                <td>06/09/2023</td>
+                <td> <img src={editar}/> <img src={lixeira}/></td>
+              </tr>
+              <tr>
+                <td>Produto 1</td>
+                <td>Descrição do Produto 1</td>
+                <td>R$ 19.99</td>
+                <td>100</td>
+                <td>06/09/2023</td>
+                <td> <img src={editar}/> <img src={lixeira}/></td>
+              </tr>
+        
+        
+        
+        
+            </tbody>
+          </table>
         </div>
       </div>
 
-      <div className="table-container container">
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Descrição</th>
-              <th>Preço</th>
-              <th>Quantidade</th>
-              <th>Atualizado</th>
-              <th>Ações</th>
-            </tr>
-          </thead>
-          <tbody>
-
-            <tr>
-              <td>Produto 1</td>
-              <td>Descrição do Produto 1</td>
-              <td>R$ 19.99</td>
-              <td>100</td>
-              <td>06/09/2023</td>
-              <td> <img src={editar}/> <img src={lixeira}/></td>
-            </tr>
-            <tr>
-              <td>Produto 1</td>
-              <td>Descrição do Produto 1</td>
-              <td>R$ 19.99</td>
-              <td>100</td>
-              <td>06/09/2023</td>
-              <td> <img src={editar}/> <img src={lixeira}/></td>
-            </tr>
-            <tr>
-              <td>Produto 1</td>
-              <td>Descrição do Produto 1</td>
-              <td>R$ 19.99</td>
-              <td>100</td>
-              <td>06/09/2023</td>
-              <td> <img src={editar}/> <img src={lixeira}/></td>
-            </tr>
-            <tr>
-              <td>Produto 1</td>
-              <td>Descrição do Produto 1</td>
-              <td>R$ 19.99</td>
-              <td>100</td>
-              <td>06/09/2023</td>
-              <td> <img src={editar}/> <img src={lixeira}/></td>
-            </tr>
-            <tr>
-              <td>Produto 1</td>
-              <td>Descrição do Produto 1</td>
-              <td>R$ 19.99</td>
-              <td>100</td>
-              <td>06/09/2023</td>
-              <td> <img src={editar}/> <img src={lixeira}/></td>
-            </tr>
-            <tr>
-              <td>Produto 1</td>
-              <td>Descrição do Produto 1</td>
-              <td>R$ 19.99</td>
-              <td>100</td>
-              <td>06/09/2023</td>
-              <td> <img src={editar}/> <img src={lixeira}/></td>
-            </tr>
-            <tr>
-              <td>Produto 1</td>
-              <td>Descrição do Produto 1</td>
-              <td>R$ 19.99</td>
-              <td>100</td>
-              <td>06/09/2023</td>
-              <td> <img src={editar}/> <img src={lixeira}/></td>
-            </tr>
-            <tr>
-              <td>Produto 1</td>
-              <td>Descrição do Produto 1</td>
-              <td>R$ 19.99</td>
-              <td>100</td>
-              <td>06/09/2023</td>
-              <td> <img src={editar}/> <img src={lixeira}/></td>
-            </tr>
-            <tr>
-              <td>Produto 1</td>
-              <td>Descrição do Produto 1</td>
-              <td>R$ 19.99</td>
-              <td>100</td>
-              <td>06/09/2023</td>
-              <td> <img src={editar}/> <img src={lixeira}/></td>
-            </tr>
-            <tr>
-              <td>Produto 1</td>
-              <td>Descrição do Produto 1</td>
-              <td>R$ 19.99</td>
-              <td>100</td>
-              <td>06/09/2023</td>
-              <td> <img src={editar}/> <img src={lixeira}/></td>
-            </tr>
-            <tr>
-              <td>Produto 1</td>
-              <td>Descrição do Produto 1</td>
-              <td>R$ 19.99</td>
-              <td>100</td>
-              <td>06/09/2023</td>
-              <td> <img src={editar}/> <img src={lixeira}/></td>
-            </tr>
-            <tr>
-              <td>Produto 1</td>
-              <td>Descrição do Produto 1</td>
-              <td>R$ 19.99</td>
-              <td>100</td>
-              <td>06/09/2023</td>
-              <td> <img src={editar}/> <img src={lixeira}/></td>
-            </tr>
-            <tr>
-              <td>Produto 1</td>
-              <td>Descrição do Produto 1</td>
-              <td>R$ 19.99</td>
-              <td>100</td>
-              <td>06/09/2023</td>
-              <td> <img src={editar}/> <img src={lixeira}/></td>
-            </tr>
-            <tr>
-              <td>Produto 1</td>
-              <td>Descrição do Produto 1</td>
-              <td>R$ 19.99</td>
-              <td>100</td>
-              <td>06/09/2023</td>
-              <td> <img src={editar}/> <img src={lixeira}/></td>
-            </tr>
-            <tr>
-              <td>Produto 1</td>
-              <td>Descrição do Produto 1</td>
-              <td>R$ 19.99</td>
-              <td>100</td>
-              <td>06/09/2023</td>
-              <td> <img src={editar}/> <img src={lixeira}/></td>
-            </tr>
-            <tr>
-              <td>Produto 1</td>
-              <td>Descrição do Produto 1</td>
-              <td>R$ 19.99</td>
-              <td>100</td>
-              <td>06/09/2023</td>
-              <td> <img src={editar}/> <img src={lixeira}/></td>
-            </tr>
-            <tr>
-              <td>Produto 1</td>
-              <td>Descrição do Produto 1</td>
-              <td>R$ 19.99</td>
-              <td>100</td>
-              <td>06/09/2023</td>
-              <td> <img src={editar}/> <img src={lixeira}/></td>
-            </tr>
-            <tr>
-              <td>Produto 1</td>
-              <td>Descrição do Produto 1</td>
-              <td>R$ 19.99</td>
-              <td>100</td>
-              <td>06/09/2023</td>
-              <td> <img src={editar}/> <img src={lixeira}/></td>
-            </tr>
-           
-           
-            
-           
-          </tbody>
-        </table>
-      </div>
     </main>
   );
 };
