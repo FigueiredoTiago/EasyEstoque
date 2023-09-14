f0e007907779278164b07c932dd9ff697e163a6d
tiago <tiagoandroloko@gmail.com>
2023-09-14 14:45:26 -0300

    modal adicionado

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,7 @@
 import "./styles.scss";
 import search from "../../assets/icons/search.png";
 import box from "../../assets/icons/box.png";
-import plus from "../../assets/icons/plus.png";
-import lixeira from '../../assets/icons/lixeira.png';
-import editar from '../../assets/icons/editar.png';
-import { Link } from "react-router-dom";
+import Create from "../Modal/Create";
 const Home = () => {
   return (
     <main className="main-home"> 
@@ -16,12 +13,17 @@ const Home = () => {
           </h1>
         
           <div className="input-box">
-            <img src={plus} className="plus" />
-            <Link to="/">Novo Produto</Link>
-            <input type="text" placeholder="Search Products..." />
-            <img src={search} alt="icon search" />
+
+            <Create />
+
+            <div className="search">
+              <input type="text" placeholder="Search Products..." />
+              <img src={search} alt="icon search" />
+            </div>
+
           </div>
         </div>
+
         <div className="table-container">
           <table>
             <thead>
@@ -41,148 +43,8 @@ const Home = () => {
                 <td>R$ 19.99</td>
                 <td>100</td>
                 <td>06/09/2023</td>
-                <td> <img src={editar}/> <img src={lixeira}/></td>
+                <td> <Create /> </td>
               </tr>
-              <tr>
-                <td>Produto 1</td>
-                <td>Descrição do Produto 1</td>
-                <td>R$ 19.99</td>
-                <td>100</td>
-                <td>06/09/2023</td>
-                <td> <img src={editar}/> <img src={lixeira}/></td>
-              </tr>
-              <tr>
-                <td>Produto 1</td>
-                <td>Descrição do Produto 1</td>
-                <td>R$ 19.99</td>
-                <td>100</td>
-                <td>06/09/2023</td>
-                <td> <img src={editar}/> <img src={lixeira}/></td>
-              </tr>
-              <tr>
-                <td>Produto 1</td>
-                <td>Descrição do Produto 1</td>
-                <td>R$ 19.99</td>
-                <td>100</td>
-                <td>06/09/2023</td>
-                <td> <img src={editar}/> <img src={lixeira}/></td>
-              </tr>
-              <tr>
-                <td>Produto 1</td>
-                <td>Descrição do Produto 1</td>
-                <td>R$ 19.99</td>
-                <td>100</td>
-                <td>06/09/2023</td>
-                <td> <img src={editar}/> <img src={lixeira}/></td>
-              </tr>
-              <tr>
-                <td>Produto 1</td>
-                <td>Descrição do Produto 1</td>
-                <td>R$ 19.99</td>
-                <td>100</td>
-                <td>06/09/2023</td>
-                <td> <img src={editar}/> <img src={lixeira}/></td>
-              </tr>
-              <tr>
-                <td>Produto 1</td>
-                <td>Descrição do Produto 1</td>
-                <td>R$ 19.99</td>
-                <td>100</td>
-                <td>06/09/2023</td>
-                <td> <img src={editar}/> <img src={lixeira}/></td>
-              </tr>
-              <tr>
-                <td>Produto 1</td>
-                <td>Descrição do Produto 1</td>
-                <td>R$ 19.99</td>
-                <td>100</td>
-                <td>06/09/2023</td>
-                <td> <img src={editar}/> <img src={lixeira}/></td>
-              </tr>
-              <tr>
-                <td>Produto 1</td>
-                <td>Descrição do Produto 1</td>
-                <td>R$ 19.99</td>
-                <td>100</td>
-                <td>06/09/2023</td>
-                <td> <img src={editar}/> <img src={lixeira}/></td>
-              </tr>
-              <tr>
-                <td>Produto 1</td>
-                <td>Descrição do Produto 1</td>
-                <td>R$ 19.99</td>
-                <td>100</td>
-                <td>06/09/2023</td>
-                <td> <img src={editar}/> <img src={lixeira}/></td>
-              </tr>
-              <tr>
-                <td>Produto 1</td>
-                <td>Descrição do Produto 1</td>
-                <td>R$ 19.99</td>
-                <td>100</td>
-                <td>06/09/2023</td>
-                <td> <img src={editar}/> <img src={lixeira}/></td>
-              </tr>
-              <tr>
-                <td>Produto 1</td>
-                <td>Descrição do Produto 1</td>
-                <td>R$ 19.99</td>
-                <td>100</td>
-                <td>06/09/2023</td>
-                <td> <img src={editar}/> <img src={lixeira}/></td>
-              </tr>
-              <tr>
-                <td>Produto 1</td>
-                <td>Descrição do Produto 1</td>
-                <td>R$ 19.99</td>
-                <td>100</td>
-                <td>06/09/2023</td>
-                <td> <img src={editar}/> <img src={lixeira}/></td>
-              </tr>
-              <tr>
-                <td>Produto 1</td>
-                <td>Descrição do Produto 1</td>
-                <td>R$ 19.99</td>
-                <td>100</td>
-                <td>06/09/2023</td>
-                <td> <img src={editar}/> <img src={lixeira}/></td>
-              </tr>
-              <tr>
-                <td>Produto 1</td>
-                <td>Descrição do Produto 1</td>
-                <td>R$ 19.99</td>
-                <td>100</td>
-                <td>06/09/2023</td>
-                <td> <img src={editar}/> <img src={lixeira}/></td>
-              </tr>
-              <tr>
-                <td>Produto 1</td>
-                <td>Descrição do Produto 1</td>
-                <td>R$ 19.99</td>
-                <td>100</td>
-                <td>06/09/2023</td>
-                <td> <img src={editar}/> <img src={lixeira}/></td>
-              </tr>
-              <tr>
-                <td>Produto 1</td>
-                <td>Descrição do Produto 1</td>
-                <td>R$ 19.99</td>
-                <td>100</td>
-                <td>06/09/2023</td>
-                <td> <img src={editar}/> <img src={lixeira}/></td>
-              </tr>
-              <tr>
-                <td>Produto 1</td>
-                <td>Descrição do Produto 1</td>
-                <td>R$ 19.99</td>
-                <td>100</td>
-                <td>06/09/2023</td>
-                <td> <img src={editar}/> <img src={lixeira}/></td>
-              </tr>
-        
-        
-        
-        
             </tbody>
           </table>
         </div>
---
+++ src/components/Modal/Create.jsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+
+import "./styles.scss";
+import plus from "../../assets/icons/plus.png";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "80vw",
+  height: "50vh",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function Create() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <h4 className="button create" onClick={handleOpen}>
+        {" "}
+        <img src={plus} /> Novo Produto{" "}
+      </h4>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
