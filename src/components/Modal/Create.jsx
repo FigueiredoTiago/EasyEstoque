09fd08c6c20a3b44ebb34d01e2de7441dab5c828
tiago <tiagoandroloko@gmail.com>
2023-09-14 15:12:53 -0300

    modal adicionado

--- src/components/Modal/Create.jsx
+++ src/components/Modal/Create.jsx
@@ -1,28 +1,49 @@
+/* eslint-disable no-unused-vars */
 import * as React from "react";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 
 import "./styles.scss";
 import plus from "../../assets/icons/plus.png";
+
+import { useForm } from "react-hook-form";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Error from "../../utils/Error";
 
 const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: "80vw",
-  height: "50vh",
-  bgcolor: "background.paper",
-  border: "2px solid #000",
+  width: "90vw",
+  maxWidth: '60rem',
+  height: "60vh",
+  bgcolor: "#f2e7dc",
   boxShadow: 24,
-  p: 4,
+  borderRadius: "1rem",
+  p: 0,
 };
 
 export default function Create() {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+
+    toast.success("Produto cadastrado com sucesso!");
+
+    reset();
+  };
 
   return (
     <div>
@@ -37,14 +58,45 @@ export default function Create() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <form onSubmit={handleSubmit(onSubmit)} className="form-modal">
+            <h2>Cadastrar Produto</h2>
+
+            <input
+              type="text"
+              {...register("name", { required: true })}
+              placeholder="Nome do Produto"
+            />
+            <input
+              type="text"
+              {...register("description", { required: true })}
+              placeholder="Descricao"
+            />
+
+            <input
+              type="text"
+              {...register("price", { required: true })}
+              placeholder="Valor"
+            />
+
+            <input
+              type="text"
+              {...register("amount", { required: true })}
+              placeholder="Quantidade"
+            />
+
+            <button type="submit">CADASTRAR</button>
+
+            <div className="error-box">
+              {errors.name && <Error error="name is required!" />}
+              {errors.description && <Error error="Description is required!" />}
+              {errors.price && <Error error="Price is required!" />}
+              {errors.amount && <Error error="Amount is required!" />}
+            </div>
+          </form>
         </Box>
       </Modal>
+
+      <ToastContainer />
     </div>
   );
 }
