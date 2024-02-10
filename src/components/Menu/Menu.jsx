d6d9bfb73b4d70153c96636b67c5be9a7e491508
Tiago Figueiredo <107004021+FigueiredoTiago@users.noreply.github.com>
2024-02-10 11:20:37 +0000

    feat(rstate menu)

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -5,7 +5,14 @@ import user from '../../assets/icons/user.png';
 import { Link } from "react-router-dom";
 import Create from "../Modal/Create";
 
+//redux
+
+import { useSelector } from 'react-redux';
+
 const Menu = () => {
+  const { data } = useSelector((state) => state.auth);
+
+
   const openMenu = () => {
     const menu = document.querySelector(".left-menu");
     menu.classList.toggle("active");
@@ -18,20 +25,20 @@ const Menu = () => {
 
       <div className="menu-content">
 
-          <div className="user-info">
-            <h3 className="username">Tiago.F</h3>
-            <p>ADMIN</p>
-          </div>
+        <div className="user-info">
+          <h3 className="username">{ data ? data.name : 'Login' }</h3>
+          <p>{data ? data.auth : '...'}</p>
+        </div>
 
-          <nav>
-            <ul>
+        <nav>
+          <ul>
 
-              <li> <Create /> </li>
-              <li> <img src={user} /> <Link>Fornecedores</Link> </li>
-              <li> <img src={logout} /><Link>Logout</Link> </li>
+            <li> <Create /> </li>
+            <li> <img src={user} /> <Link>Fornecedores</Link> </li>
+            <li> <img src={logout} /><Link>Logout</Link> </li>
 
-            </ul>
-          </nav>
+          </ul>
+        </nav>
 
       </div>
 
